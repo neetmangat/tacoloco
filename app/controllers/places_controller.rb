e855a15a807b2b136a745c4085c49759d3732965
neetmangat <neetmangat@gmail.com>
2020-05-18 11:34:30 +0000

    Error Messages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -29,7 +29,11 @@ class PlacesController < ApplicationController
         end
 
         @place.update_attributes(place_params)
-        redirect_to root_path
+        if @place.valid?
+            redirect_to root_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
     end
 
     def destroy
@@ -43,8 +47,12 @@ class PlacesController < ApplicationController
     end
 
     def create
-        current_user.places.create(place_params)
-        redirect_to root_path
+        @place = current_user.places.create(place_params)
+        if @place.valid?
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
 
     private
