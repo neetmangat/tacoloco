48120e6a6947cb5472b16afab442c5625e730d3e
neetmangat <neetmangat@gmail.com>
2020-05-25 01:57:48 +0000

    heroku fix 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,26 +1,26 @@
 Rails.application.configure do
 
-  #ActionMailer::Base.smtp_settings = {
-  #  address: 'smtp.sendgrid.net',
-  #  port: '587',
-  #  authentication: :plain,
-  #  user_name: ENV['SENDGRID_USERNAME'],
-  #  password: ENV['SENDGRID_PASSWORD'],
-  #  domain: 'heroku.com',
-  #  enable_starttls_auto: true
-  #}
-
   ActionMailer::Base.smtp_settings = {
-    :address => "smtp.sendgrid.net",
-    :port => 465,
-    :domain => "vibol.xyz",
-    :ssl => true,
-    :enable_starttls_auto => true,
-    :authentication => :login,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain: 'heroku.com'
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
   }
+
+  #ActionMailer::Base.smtp_settings = {
+  #  :address => "smtp.sendgrid.net",
+  #  :port => 465,
+  #  :domain => "vibol.xyz",
+  #  :ssl => true,
+  #  :enable_starttls_auto => true,
+  #  :authentication => :login,
+  #  :user_name => ENV['SENDGRID_USERNAME'],
+  #  :password => ENV['SENDGRID_PASSWORD'],
+  #  :domain: 'heroku.com'
+  #}
 
   config.action_mailer.default_url_options = { host:'nomster-neet-mangat.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
