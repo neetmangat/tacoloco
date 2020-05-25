13f1447ffc90c4251899b56a8462b924aa78fb25
neetmangat <neetmangat@gmail.com>
2020-05-25 08:40:30 +0000

    forget emailing

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,14 @@
 class NotificationMailer < ApplicationMailer
     default from: "no-reply@nomsterapp.com"
   
-    def comment_added(comment)
-        @place = comment.place 
-        @place_owner = @place.user
+#    def comment_added(comment)
+#        @place = comment.place 
+#        @place_owner = @place.user
 
-        mail(to: @place_owner.email, 
-             subject: "A comment has been added to #{@place.name}")
-    end
+#        mail(to: @place_owner.email, 
+#             subject: "A comment has been added to #{@place.name}")
+#    end
     
+    def comment_added
+    end 
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,14 @@
 Rails.application.configure do
 
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
-  }
+  #ActionMailer::Base.smtp_settings = {
+  #  address: 'smtp.sendgrid.net',
+  #  port: '587',
+  #  authentication: :plain,
+  #  user_name: ENV['SENDGRID_USERNAME'],
+  #  password: ENV['SENDGRID_PASSWORD'],
+  #  domain: 'herokuapp.com',
+  #  enable_starttls_auto: true
+  #}
 
   #ActionMailer::Base.smtp_settings = {
   #  :address => "smtp.sendgrid.net",
@@ -19,7 +19,7 @@ Rails.application.configure do
   #  :authentication => :login,
   #  :user_name => ENV['SENDGRID_USERNAME'],
   #  :password => ENV['SENDGRID_PASSWORD'],
-  #  :domain: 'heroku.com'
+  #  :domain: 'herokuapp.com'
   #}
 
   config.action_mailer.default_url_options = { host:'nomster-neet-mangat.herokuapp.com' }
