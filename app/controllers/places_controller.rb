e4da3f5f07eed40c0ef00b5358d05bf4d1f14e34
neetmangat <neetmangat@gmail.com>
2020-05-30 10:47:59 +0000

    Fixing production maps issue

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -51,7 +51,7 @@ class PlacesController < ApplicationController
     def create
         @place = current_user.places.create(place_params)
         if @place.valid?
-            redirect_to root_path
+            redirect_to place_path(@place)
         else
             render :new, status: :unprocessable_entity
         end
