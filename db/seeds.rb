4d292cf63a68c433a6860a41dda7f693b7c1cf41
neetmangat <neetmangat@gmail.com>
2020-06-18 02:09:45 +0000

    Cleaned up files and reset db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
-Photo.create!([
-  {caption: "The front of Velvet Taco", place_id: 1, picture: "velvet_taco.jpg", user_id: 2}
-])
 User.create!([
   {email: "neetmangat@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
   {email: "cuzim2nyce@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
 ])
-Comment.create!([
-  {message: "Love", rating: "3_stars", user_id: 2, place_id: 1},
-  {message: "Seriously, it rocks", rating: "5_stars", user_id: 1, place_id: 1},
-  {message: "If you haven't tried it, you need to go today", rating: "4_stars", user_id: 1, place_id: 1}
-])
+
 Place.create!([
   {name: "Velvet Taco", description: "Craft Tacos so good", address: "3411 McKinney Ave, Dallas, TX 75204", user_id: 1, latitude: 32.8056021, longitude: -96.7988702},
   {name: "Taco Planet", description: "The best taco food truck ever", address: "1411 Westheimer Rd, Houston, TX 77006", user_id: 1, latitude: 29.7429567, longitude: -95.3962661},
@@ -21,3 +14,13 @@ Place.create!([
   {name: "Roti Mediterranean", description: "Amazing Mediterranean Food", address: "6602 Fannin St, Houston, TX 77030", user_id: 1, latitude: 29.7096621, longitude: -95.4012496},
   {name: "Taco Bell", description: "Who can ignore the classic?!?", address: "13670 Preston Rd, Dallas, TX 75240", user_id: 2, latitude: 32.9342085, longitude: -96.803321}
 ])
+
+Comment.create!([
+  {message: "Love", rating: "3_stars", user_id: 2, place_id: 1},
+  {message: "Seriously, it rocks", rating: "5_stars", user_id: 1, place_id: 1},
+  {message: "If you haven't tried it, you need to go today", rating: "4_stars", user_id: 1, place_id: 1}
+])
+
+#Photo.create!([
+#  {caption: "The front of Velvet Taco", place_id: 1, picture: "velvet_taco.jpg", user_id: 2}
+#])
